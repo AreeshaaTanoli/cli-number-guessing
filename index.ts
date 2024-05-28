c24dd342cad8792eb385f2ca3dff3b5fc1b18c01
AreeshaaTanoli <areeshaa942@gmail.com>
2024-05-28 13:07:01 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+// 1) computer will generate a random number... (done)
+
+// 2) user imput for guessing number.... (done)
+
+// 3) compare using input with computer generated number and show result.... (done)
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+// console.log(randomNumber);
+
+const answers =  await inquirer.prompt([
+    {
+        name : "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6:"
+
+},
+]);
+
+if (answers.userGuessedNumber === randomNumber){
+    console.log("Congratulations! you guessed right number.")
+}
+else{
+    console.log("You guessed wrong number.");
+};
